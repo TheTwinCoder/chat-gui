de410ed3c598f7ff0f3fa33f95b738387526ae16
SHIM-JINSEO <sjs011828@gmail.com>
2025-05-17 20:27:50 +0900

    feat: add Loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,8 @@
+// components/Loading.tsx
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-[20px] h-[20px] border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+}
